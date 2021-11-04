6eb2023b4b31dc5e6122cbef803ab93ba0d29daf
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2021-11-04 18:26:22 +0000

    Add notes on reference and value types

--- pointergotcha/main.go
+++ pointergotcha/main.go
@@ -8,6 +8,11 @@ func main() {
 	updateSlice(mySlice)
 
 	fmt.Println(mySlice)
+	// outputs change to "Bye" because slices are pointers to arrays really
+	// and the pointer gets copied but points to the same array
+
+	// Slices are called reference types, so are maps, channels, pointers and functions
+	// Value types are int, float, string, bool and structs
 }
 
 func updateSlice(s []string) {
